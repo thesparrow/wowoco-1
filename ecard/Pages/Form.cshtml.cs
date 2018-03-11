f13ba5b540995038a91eb30b9fa502a2a9239d9c
Camille Alcasid <camille@westsidewebsites.com>
2018-03-10 19:23:29 -0800

    .

--- ecard/Pages/Form.cshtml.cs
+++ ecard/Pages/Form.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using ecard.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
 namespace ecard.Pages
 {
@@ -31,9 +33,91 @@ namespace ecard.Pages
 
         }
 
-        public void OnGet()
+
+        [HttpPost]
+        public async Task<IActionResult> OnPost()
         {
 
+            if (await isValid())
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+                        // DB Related Customized values added with each record
+                        //_myGreetings.CreateDate = DateTime.Now.ToString();
+                        //_myGreetings.CreateIP = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+                        //Clean Data before insertion 
+                        _myGreetings.senderemail = _myGreetings.senderemail.ToLowerInvariant();
+                        _myGreetings.friendemail = _myGreetings.friendemail.ToLowerInvariant();
+
+                        // DB Related add record
+                        _myDbBridge.Greetings.Add(_myGreetings);
+                        _myDbBridge.SaveChanges();
+
+                        //REDIRECT to the page with a new operator (name/value pair)
+                        return RedirectToPage("Preview", new { id = _myGreetings.ID });
+                    }
+
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                        return RedirectToPage("Index");
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("_myGreetings.reCaptcha", "Please verify you're not a robot!");
+            }
+
+            return Page();
+
         }
+
+        /**
+         * reCAPTHCA SERVER SIDE VALIDATION 
+         * 
+         *      Create an HttpClient and store the the secret/response pair
+         *      Await for the sever to return a json obect 
+         * */
+        private async Task<bool> isValid()
+        {
+            var response = this.HttpContext.Request.Form["g-recaptcha-response"];
+            if (string.IsNullOrEmpty(response))
+                return false;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    var values = new Dictionary<string, string>();
+                    //values.Add("secret", "6LfVpjEUAAAAAK0FdygAgh0P1gZ8QU24ildwT86r");
+                    values.Add("secret", _myConfiguration["ReCaptcha:PrivateKey"]);
+
+                    values.Add("response", response);
+                    //values.Add("remoteip", this.HttpContext.Connection.RemoteIpAddress.ToString()); 
+
+                    var query = new FormUrlEncodedContent(values);
+
+                    var post = client.PostAsync("https://www.google.com/recaptcha/api/siteverify", query);
+
+                    var json = await post.Result.Content.ReadAsStringAsync();
+
+                    if (json == null)
+                        return false;
+
+                    var results = JsonConvert.DeserializeObject<dynamic>(json);
+
+                    return results.success;
+                }
+
+            }
+            catch { }
+
+            return false;
+        }
+
     }
 }
